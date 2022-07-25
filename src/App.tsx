bf129d225f36ade83b21173a32a286214fc48dae
Keadeish <keadeish10@hotmail.com>
2022-07-25 16:42:14 +0100

    formatted

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import { greet } from "./utils/greet";
-import MapBabyNames from './BabyUI'
-import './app.css'
+import MapBabyNames from "./BabyUI";
+import "./app.css";
 
 function App(): JSX.Element {
-  return  <MapBabyNames />
+  return <MapBabyNames />;
 }
 
 export default App;
--- src/BabyUI.tsx
+++ src/BabyUI.tsx
@@ -1,23 +1,26 @@
-import Babies from './babyNames.json'
+import Babies from "./babyNames.json";
 
-const babies2 = [...Babies].sort((a, b)=> a.name>b.name? 1 : -1)
+const babies2 = [...Babies].sort((a, b) => (a.name > b.name ? 1 : -1));
 
-console.log(babies2)
+console.log(babies2);
 
-function MapBabyNames():JSX.Element {
-
-
-    const babiesMapped = babies2.map(baby => {
-        if(baby.sex == "f") {
-            return( <button className='girls' key={ baby.id }>{baby.name}</button>
-            )}
-        else {
-            return( <button className='boyz' key={ baby.id }>{baby.name}</button>
-            )
-
-        }
-    })
-    return <b>{babiesMapped}</b>
+function MapBabyNames(): JSX.Element {
+  const babiesMapped = babies2.map((baby) => {
+    if (baby.sex === "f") {
+      return (
+        <button className="girls" key={baby.id}>
+          {baby.name}
+        </button>
+      );
+    } else {
+      return (
+        <button className="boyz" key={baby.id}>
+          {baby.name}
+        </button>
+      );
+    }
+  });
+  return <b>{babiesMapped}</b>;
 }
 
-export default MapBabyNames
+export default MapBabyNames;
